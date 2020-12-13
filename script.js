23ddeea44d16332d1e0d95a183a2c5364ba6c438
Hector Diaz <hectordiaz1103@gmail.com>
2020-12-13 17:25:00 -0500

    completely changed my original code, uploading my 4th attempt. this is the closest. I believe the only thing left to do is return the password

--- script.js
+++ script.js
@@ -7,83 +7,63 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+//variables
+var pwlenght
+var upperCon
+var lowerCon
+var numberCon
+var symbolCon
+//ask for lenght of password
+pwlenght = prompt("how long would you like your password to be, between 8-128?")
+if (pwlenght < 8 || pwlenght > 128){
+  prompt ("must be between 8-128")
+}
+else {
+  alert("nice lenght selection");
+}
+  //convert to a number
+  pwlenght = +pwlenght;
+  console.log(typeof(pwlenght));
+  console.log(pwlenght);
+// ask if these characters are wanted 
+upperCon = confirm("do you want to use upper case?")
+lowerCon = confirm("do you want to use lower case?")
+numberCon = confirm("do you want to use numbers?")
+symbolCon = confirm("do you want to use symbols?")
+console.log(upperCon,lowerCon, numberCon, symbolCon);
+//add to password if wanted
+if (upperCon){
+  userpw = "QWERTYUIOPASDFGHJKLZXCVBNM"
+  console.log(userpw);
+}
+//add to password if wanted
+if (lowerCon){
+  userpw += "qwertyuiuiopasdfghjklzxcvbnm"
+  console.log(userpw);
+}
+//add to password if wanted
+if (numberCon){
+  userpw += "1234567890"
+  console.log(userpw);
+}
+if (symbolCon){
+  userpw += "!@#$%^&*()"
+  console.log(userpw);
+}
+//add to password if wanted
+for (var i = 0; i < pwlenght; i++){
+  password = userpw[Math.floor(Math.random() * userpw.length - 1)]
+  console.log(password);
+}
+//return password
+
+
+var userpw = ""
+var password = ""
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// GIVEN I need a new, secure password
 
-  function generatePassword(){
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
- 
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-var passwordlenght = prompt("How long do you want your password to be?");
-console.log(passwordlenght);
-// Need an if statement to assure password is between 8-128 characters
-
-
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-var specialchar = confirm("Do you want a special charecter in your password");
-var uppercase = confirm("Do you want an uppercase in your password?");
-var lowercase = confirm("Do you want an lowercase in your password?");
-var number = confirm("Do you want numbers in your password?");
-console.log(specialchar);
-console.log(uppercase);
-console.log(lowercase);
-console.log(number);
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// We need a character array
-// We need a uppercase array
-// We need a lowercase array
-// We need a number array
-// We need an array to hold user selected options
-var characterarray = ["!", "@","#", "$", "%", "^", "&", "*"];
-var uppercasearray = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
-var lowercasearray = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
-var numbersarray = ["1","2","3","4","5","6","7","8","9","0"];
-var totalarray = []
-if (specialchar===true){
-  totalarray = totalarray.concat(characterarray);
-}
-if (uppercase===true){
-  totalarray = totalarray.concat(uppercasearray);
-}
-if (lowercase===true){
-  totalarray = totalarray.concat(lowercasearray);
-}
-if (number===true){
-  totalarray= totalarray.concat(numbersarray);
-}
-//create a for loop based on the number selected by user 
-
-
-//need math floor and math random to select randomly from total array 
-for (i = 0; i < "passwordlength"; i++);{
-  var generatedPassword = totalarray[Math.floor(Math.random())];
-}
-console.log(generatedPassword);
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-  }
-
-  
-
-
-
-
- 
